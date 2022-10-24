10f1ce0ddc957b98015f07e38ec1c37f3afc1ce6
monkhzul <38126118+monkhzul@users.noreply.github.com>
2022-10-24 08:29:44 +0800

    commit

--- booking/pages/api/insert/insert.js
+++ booking/pages/api/insert/insert.js
@@ -7,6 +7,7 @@ export default async function insert(req, res) {
   const leftStatus = req.body.leftStatus;
   const rightStatus = req.body.rightStatus;
   const date = req.body.date;
+  
   const data = await prisma.$queryRaw`INSERT INTO [dbo].[Hall] ([time] ,[leftStatus],[rightStatus],[date]) 
   VALUES (${time}, ${leftStatus}, ${rightStatus}, ${date})`
   res.status(200).json(data)
